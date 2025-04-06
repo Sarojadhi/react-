1fe3c367dafdd91ca849f08e035c498b81daacbe
sarojadh1111 <sarojadh1111@gmail.com>
2025-04-06 10:27:50 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import Footer from "./Footer";
 function App() {
-
   return (
-  <>
+    <>
       <div>
-        <Navbar/>
-        </div>
+        <Footer />
+      </div>
     </>
   );
 }
-export default App ;
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <Footer className="content">
+          {" "}
+          © 2025{" "}
+          <span className="font-semibold text-yellow-400">EVEREST YAK </span>.
+          All Rights Reserved.
+        </Footer>
+      </div>
+    </>
+  );
+}
+export default Footer;
